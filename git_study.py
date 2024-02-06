7630f46a7f9beb0da870a354618c9727b87ae915
RuslanSMP <159168119+RuslanSMP@users.noreply.github.com>
2024-02-06 21:50:31 +0400

    Update from another user

--- git_study.py
+++ git_study.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('And I change it again...')
+    print('hello from another user')
 
 
 if __name__ == '__main__':
