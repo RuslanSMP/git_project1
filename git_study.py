ba45a5f5278a4f3736591c10e9be9eda44a845b7
user <user@mail.ru>
2024-02-11 18:01:16 +0400

    add_superfunction

--- git_study.py
+++ git_study.py
@@ -1,8 +1,13 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('And I change it again...')
     print('hello from another user')
+    my_superfunction()
 
 
 if __name__ == '__main__':
